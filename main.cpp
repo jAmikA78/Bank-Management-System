327eb6b3d77578fda21c8f0097e3b6a79077b36b
jamika78 <ahmed.ibrahim7893@gmail.com>
2023-12-20 14:55:13 +0200

    add some feuters

--- main.cpp
+++ main.cpp
@@ -1,25 +1,25 @@
 /*
 Project Description:-
 This project manages the bank activities. The bank has many branches. Each
-branch has basic information (ID, Name, Manager). The bank deals with many
-account holders and each holder may have many accounts. Each holder has
+Branch has basic information (ID, Name, Manager). The bank deals with many
+account holders and each Holder may have many accounts. Each Holder has
 basic information (ID, Name, Address, Balance). Create a C++ program using
 two singly Linked Lists to perform the following functions.
 
 1- For Branches:-
-a. Add new branch in sorted order bi id.
+a. Add new Branch in sorted order bi id.
 b. Display Branch.
 c. Search for Branch by ID.
 d- Remove Branch.
 
 2- For Account Holders:-
-a. Add new holder in sordet holder by balance.
-b. Remove a holder.
-c. Display holder data.
-d. Search for holder by Name.
+a. Add new Holder in sordet Holder by balance.
+b. Remove a Holder.
+c. Display Holder data.
+d. Search for Holder by Name.
 e. Update Holder Information.
 f. Given a Branch id, display Holders in that Branch.
-g. Display holders of a branch ordered by their balances.
+g. Display holders of a Branch ordered by their balances.
 
 */
 
@@ -33,32 +33,67 @@ g. Display holders of a branch ordered by their balances.
 using namespace std;
 
 // define bransh struct
-struct branch
+struct Branch
 {
     string id, name, manager;
-    branch *next;
+    Branch *next;
+    Branch(string ID, string Name, string Manager)
+    {
+        id = ID;
+        name = Name;
+        manager = Manager;
+        next = NULL;
+    }
 };
 
-// define holder struct
-struct holder
+// define Holder struct
+struct Holder
 {
     string branch_id, id, name, address;
     double balance;
-    holder *next;
+    Holder *next;
+
+    Holder(string B_id, string ID, string Name, string Address, double Balance)
+    {
+        branch_id = B_id;
+        id = ID;
+        name = Name;
+        address = Address;
+        balance = Balance;
+        next = NULL;
+    }
 };
 
 struct list_of_branshs
 {
-    branch *head;
-
-   
+    Branch *head;
+    Branch *last;
+    // add new branch (in sorted order by id)
+    void add_branch(string ID, string Name, string Manager)
+    {
+        Branch *temp = new Branch(ID, Name, Manager);
+        if (head == NULL || temp->id < head->id)
+        {
+            temp->next = head;
+            head = temp;
+            if (last == NULL)
+                last = temp;
+            return;
+        }
+        Branch *cur = head;
+        while (cur->next != NULL && cur->next->id < temp->id)
+            cur = cur->next;
+        temp->next = cur->next;
+        cur->next = temp;
+        if (temp->next == NULL)
+            last = temp;
+    }
 };
 
 struct list_of_holders
 {
-    holder *head;
-
-    
+    Holder *head;
+    Holder *last;
 };
 
 // function to useing system
@@ -131,6 +166,7 @@ void store_holders_data()
     // Close holders_data
     holders_data.close();
 }
+
 // main function
 
 int main()
@@ -142,4 +178,3 @@ int main()
     store_holders_data();
     return 0;
 }
-
